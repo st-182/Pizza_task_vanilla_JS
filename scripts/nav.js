43ad8e89b5974547ac959cc44e3e28cc402e7cee
L.A <80128663+lukeadam1212@users.noreply.github.com>
2021-10-11 23:48:44 +0300

    1

---
+++ scripts/nav.js
@@ -0,0 +1,28 @@
+//! variables
+
+const clickBtn = document.querySelector(`#fa`);
+const menu = document.querySelector(`.dropdown`);
+const copyright = document.querySelector(`.copyright`);
+const yearNow = new Date().getFullYear();
+//! functions
+// option 1
+// const toggleMenu = () => menu.classList.toggle(`active`);
+
+// option 2
+let showMenu = false;
+
+const toggleMenu = () => {
+  if (showMenu) {
+    menu.style.display = `none`;
+    showMenu = false;
+  } else {
+    menu.style.display = `flex`;
+    showMenu = true;
+  }
+};
+
+copyright.innerHTML += yearNow + ` &copy LOBO'S car wash all rights reserved`;
+
+//! Events
+
+clickBtn.addEventListener(`click`, toggleMenu);
